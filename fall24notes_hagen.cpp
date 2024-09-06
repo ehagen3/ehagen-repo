6418d98baecc99d6f7806116cb93a15f611a6dbf
Esteban Hagen <ehagen3@saddleback.edu>
2024-09-05 18:59:13 -0700

    9.5 notes

---
+++ fall24notes_hagen.cpp
@@ -0,0 +1,11 @@
+<VARIABLES> 
+
+// Variable is a memory location that we nname it to "x" ;
+// Variable should hae a type which is the typ of the data that we will store in that memory location
+// .. A few possible types
+  - int .. // example : 100, -1000 [ 4 bytes ]
+  - float ..   example : 11.9 , -9.3 [ 4 bytes ]
+  - double ..  same as float with higher precision
+  - char ..    example : 'a' , 'b' , 'c' 
+  - string ..  example : "hello there my friend!"
+  - bool ..    example : true, false
